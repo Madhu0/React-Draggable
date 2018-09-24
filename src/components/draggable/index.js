dea2ae0db1f8d0e0fdaffbec9ef3d9134ba88586
Madhusudhan Reddy K <kmadhusudhanreddy3@gmail.com>
2018-09-24 20:53:19 +0530

    Basic testcases are done

--- src/components/draggable/index.js
+++ src/components/draggable/index.js
@@ -1,17 +1,33 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import { Consumer } from '../wrapper';
 
-export default class Draggable extends Component {
+class Draggable extends Component {
 
   constructor(props) {
     super(props);
     this.id = (new Date()).getTime() + '';
+    this.state = {
+      itemMoved: false
+    };
+  }
+
+  handleDragEnd = (e) => {
+    console.log(e);
+    if (e.dataTransfer.dropEffect !== 'none') {
+      this.setState({
+        itemMoved: true,
+      });
+      this.props.eventHandlers.onDragEnd('adcdefgh');
+    }
   }
 
   handleDragStart = (e) => {
     // console.log(e);
     // This should customizable, whether a image link from prop or component from prop
     const { dragImageLink, dragComponent, isDomNode = false } = this.props;
+    e.dataTransfer.setData('text/plain', 'adcdefgh');
+    this.props.eventHandlers.onDragStart('adcdefgh', this.props.children);
     let dragComp;
     if (dragImageLink) {
       dragComp = document.createElement('img');
@@ -31,9 +47,13 @@ export default class Draggable extends Component {
 
   render() {
     const { children, className } = this.props;
-    return (<div className={className} draggable={true} onDragStart={this.handleDragStart}>
+    return (<div className={className} draggable={true} onDragStart={this.handleDragStart} onDragEnd={this.handleDragEnd}>
       <div id={`dragImage-${this.id}`} style={{ transform: 'translate(-1000px)', position: "absolute", top: '-1000px', left: '-1000px' }}></div>
-      {children}
+      {!this.state.itemMoved && <div id="12345">{children}</div>}
     </div>);
   }
 }
+
+export default (props) => (<Consumer>
+  {(handlers) => <Draggable {...props} eventHandlers={handlers}/>}
+</Consumer>);
