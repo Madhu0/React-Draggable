48ce0d0117c226a395b3ccdb35c6e1eff3dd4edf
Madhusudhan Reddy <kmadhusudhanreddy3@gmail.com>
2018-10-06 00:15:01 +0530

    Making eventListners property private organizer

--- src/components/event/index.js
+++ src/components/event/index.js
@@ -1,26 +1,32 @@
-export default class EventOrganizer {
-  constructor(eventsList) {
-    this.eventListeners = {};
-    this.init(eventsList);
-  }
+var Wrapper = (eventsList) => {
+  // Shoudn't expose this object
+  eventListeners = {};
+  class EventOrganizer {
+    constructor(eventsList) {
+      this.init(eventsList);
+    }
 
-  init = (list) => {
-    list.forEach((item) => {
-      this.eventListeners[item] = {};
-    });
-  }
+    init = (list) => {
+      list.forEach((item) => {
+        eventListeners[item] = {};
+      });
+    }
 
-  addEventListener = (event, callback) => {
-    const key = (new Date()).getTime();
-    this.eventListeners[event][key] = callback;
-    return () => {
-      delete this.eventListeners[event][key];
+    addEventListener = (event, callback) => {
+      const key = (new Date()).getTime();
+      eventListeners[event][key] = callback;
+      return () => {
+        delete eventListeners[event][key];
+      }
+    }
+
+    dispatchEvent = (event, data) => {
+      Object.keys(eventListeners[event]).forEach((callback) => {
+        callback(data);
+      });
     }
   }
-
-  dispatchEvent = (event, data) => {
-    Object.keys(this.eventListeners[event]).forEach((callback) => {
-      callback(data);
-    });
-  }
+  Wrapper.prototype = new EventOrganizer();
 }
+
+export default Wrapper
